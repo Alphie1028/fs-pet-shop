406f112c2a57cd32a4d37068bb1aa9a2dded09b4
alphie1028 <alphie1028@gmail.com>
2023-02-17 16:20:57 -0500

    expressServer func

--- expressServer.js
+++ expressServer.js
@@ -2,17 +2,32 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const port = 8000;
-app.get('/pets', function(req, res){
-    fs.readFile('./pets.json', function(err, data){
+app.get('/pets/:id?', function(req, res, next){
+    fs.readFile('./pets.json', function(err, data, next){
         if(err){
-            res.send('Nope')
+            next();
         }else{
             allPets = JSON.parse(data);
-            res.send(allPets)  
+            if(req.params.id){
+                let index = parseInt(req.params.id);
+                let chosenPet = allPets[index];
+                if(chosenPet){
+                    res.send(chosenPet);
+                }else{
+                    res.status(404).send('Pet not found.');
+                }  
+            }else{
+                res.send(allPets);   
+            }
         }
     })
-    
 })
+
+app.use((req, res, next)=>{
+    res.status(404).send('Page not found');
+})
+
+
 
 app.listen(port, ()=>{
     console.log(`listening to port ${port}`);
